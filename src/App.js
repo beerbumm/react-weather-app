1211244e9e7f8f6874f3fac22770813c8ae855fe
beerbumm <chelseaw.gfx@gmail.com>
2023-11-07 14:36:51 -0800

    added GitHub repo link

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <small>
+        <a
+          href="https://github.com/beerbumm/weather-app"
+          target="_blank"
+          id="github-link"
+        >
+          Open source code{" "}
+        </a>
+        by Chelsea Watson
+      </small>
+    </div>
+  );
 }
 
 export default App;
