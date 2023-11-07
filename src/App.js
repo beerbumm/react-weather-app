cd88a45bfa6d215d4103bd4cb02d3a97c7214698
beerbumm <chelseaw.gfx@gmail.com>
2023-11-07 15:16:35 -0800

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
+import Weather from "./Weather";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <small>
-        <a
-          href="https://github.com/beerbumm/weather-app"
-          target="_blank"
-          id="github-link"
-        >
-          Open source code{" "}
-        </a>
-        by Chelsea Watson
-      </small>
+      <div className="container">
+        <Weather />
+        <small>
+          <a
+            href="https://github.com/beerbumm/weather-app"
+            target="_blank"
+            rel="noreferrer"
+            id="github-link"
+          >
+            Open source code{" "}
+          </a>
+          by Chelsea Watson
+        </small>
+      </div>
     </div>
   );
 }
